14c1e8f4c4eba726871dd4ac890591d7d2236f2e
Kcits970 <geometricstick@gmail.com>
2026-07-23 22:28:59 +0900

    Feat: create a header file for bitmap functions

---
+++ include/danux/bitmap.h
@@ -0,0 +1,10 @@
+#ifndef DANUX_BITMAP_H
+#define DANUX_BITMAP_H
+
+#include <stdint.h>
+
+extern void bitmap_init(void);
+extern void *bitmap_alloc(uint64_t);
+extern void bitmap_free(void *, uint64_t);
+
+#endif
