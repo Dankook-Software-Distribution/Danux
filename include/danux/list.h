bb9e6593226e31fd264b350336600cc0c8389822
Kcits970 <geometricstick@gmail.com>
2026-08-04 13:19:23 +0900

    Feat: add function list_empty to check emptiness of the list

--- include/danux/list.h
+++ include/danux/list.h
@@ -1,6 +1,8 @@
 #ifndef DANUX_LIST_H
 #define DANUX_LIST_H
 
+#include <stdbool.h>
+
 // Circular doubly linked-list implementation.
 struct list_head {
 	struct list_head *prev, *next;
@@ -13,6 +15,11 @@ static inline void list_head_init(struct list_head *node) {
 static inline void list_head_link(struct list_head *node1, struct list_head *node2) {
 	node1->next = node2;
 	node2->prev = node1;
+}
+
+// A list is empty if it is a singleton.
+static inline bool list_empty(struct list_head *node) {
+	return node->next == node;
 }
 
 extern void list_add_prev(struct list_head *, struct list_head *);
